a2c414510cd19995d200484a0814aa6b2cccc0cb
Abid Majeed <abidmajeed610@gmail.com>
2025-08-25 16:08:16 +0400

    Updated mobile on browser issue

--- script.js
+++ script.js
@@ -355,7 +355,7 @@ function detectInAppBrowser() {
         top: 0;
         left: 0;
         width: 100%;
-        height: 100vh;
+        height: 100%;
         min-height: 120px;
         background: #111;
         display: flex;
